72599e0782e0fba318fc0c4e044a2293c958e242
Tejas Powar <tejasvpowar@gmail.com>
2020-08-24 16:39:47 +0530

    Updates

---
+++ precache-manifest.fbe616c19e9406fafcf303a26414cc41.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00075f0387282fefd43c9ff5f5d39e4",
+    "url": "/Project_Dashboard/index.html"
+  },
+  {
+    "revision": "0aba77ce9848c1b30cdd",
+    "url": "/Project_Dashboard/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "cd843896ff123f00d6e5",
+    "url": "/Project_Dashboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0aba77ce9848c1b30cdd",
+    "url": "/Project_Dashboard/static/js/2.f1df61a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Project_Dashboard/static/js/2.f1df61a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd843896ff123f00d6e5",
+    "url": "/Project_Dashboard/static/js/main.9b22d589.chunk.js"
+  },
+  {
+    "revision": "dcd1d72c51432bdf69cb",
+    "url": "/Project_Dashboard/static/js/runtime-main.c805b0fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Project_Dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
